819988a539e84496136c95b1b68552b0a117c4a4
JonasRdeveloper <gamerdeveloper478@gmail.com>
2025-04-17 14:04:34 -0300

    Melhorias feitas

--- src/Roteiros/apresentacao.gd
+++ src/Roteiros/apresentacao.gd
@@ -1,10 +1,22 @@
 extends Node
 
 @export_file('*.dtl') var linha_do_tempo
+@onready var titulo := get_node("Controle_de_apresentacao/Titulo") as RichTextLabel
+@onready var fundo_fim := get_node("Controle_de_apresentacao/fundo_fim") as ColorRect
+
 
 func _ready():
 	Dialogic.start(linha_do_tempo)
+	Dialogic.signal_event.connect(_ligar_titulo_do_jogo)
+	Dialogic.signal_event.connect(_ligar_fim_da_apresentacao)
 	
-func _process(_delta):
-	print_debug(Dialogic.VAR.informacao.to_lower())
-	print_debug(Dialogic.VAR.Dia.to_lower())
+func _ligar_titulo_do_jogo(titulo_jogo: String) -> void:
+	if titulo_jogo == 'Aparecer':
+		while titulo.visible_ratio < 1:
+			await get_tree().create_timer(0.05).timeout
+			titulo.visible_characters += 1
+	
+func _ligar_fim_da_apresentacao(fim: String) -> void:
+	if fim == 'final':
+		%Particulas_de_coracao.set_emitting(true)
+		
